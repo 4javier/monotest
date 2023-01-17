36f87fbd1b3db7cfcffd9f9e34d2cae9d03f1c94
4javier <4javier4@gmail.com>
2023-01-17 18:48:11 +0100

    Initial commit

---
+++ apps/to-be-del/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'monotest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'to-be-del';
+}
